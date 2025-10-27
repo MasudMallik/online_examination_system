ca09671815401e2c1c4b40be80cb683946ae9e9e
MasudMallik <masudmallik96@gmail.com>
2025-10-27 15:15:31 +0530

    create login page

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI,Cookie,Request,Form
+from fastapi.responses import RedirectResponse,HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app=FastAPI()
+app.mount("/static",StaticFiles(directory="static"),name="static")
+template=Jinja2Templates(directory="templates")
+
+@app.get("/",response_class=HTMLResponse)
+def login(request:Request):
+    return template.TemplateResponse("login.html",{"request":request})
+@app.post("/")
+def get_data(request:Request,email:str=Form(...),password:str=Form(...)):
+    print(email,password)
